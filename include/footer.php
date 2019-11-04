6f1ea0a42fe5d52de63f0558be09bcd017a518ec
Cedric <cedric_elong@yahoo.it>
2019-11-04 22:41:52 +0100

    My Website Initialization

---
+++ include/footer.php
@@ -0,0 +1,4 @@
+<div class="footer">
+    <div class="text-center"><small><i class="fa fa-copyright" aria-hidden="true">Made by Cedric Elong © 2019</i></small></div>
+    <div class="text-center"><a href="https://twitter.com/cedric_elong" target="_blank" rel="noopener noreferrer">twitter</a></div>
+</div>
