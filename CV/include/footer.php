358c5e15d73a5d0c5fb05bb7a097738442e759bb
Cedric <cedric_elong@yahoo.it>
2019-11-06 00:36:56 +0100

    My website

--- CV/include/footer.php
+++
@@ -1,4 +0,0 @@
-<div class="footer">
-    <div class="text-center"><small><i class="fa fa-copyright" aria-hidden="true">Made by Cedric Elong © 2019</i></small></div>
-    <div class="text-center"><a href="https://twitter.com/cedric_elong" target="_blank" rel="noopener noreferrer">twitter</a></div>
-</div>
